dd83be70ed605ad1ba0940b678282b134a6f9b1a
Junyoung Choi <fluke8259@gmail.com>
2017-04-16 22:12:58 +0900

    Add request logger

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 const proxy = require('http-proxy-middleware')
+const logger = require('morgan')
 
 app.prepare()
   .then(() => {
     const server = express()
+
+    const logTarget = dev
+      ? /^(?!\/_next\/on-demand-entries-ping).+/
+      : '*'
+    server.use(logTarget, logger('dev'))
 
     server.use('/api', proxy('http://127.0.0.1:3001'))
     server.use('/auth', proxy('http://127.0.0.1:3001'))
