2923f16bed33b8728d8170ec4f9b681c1ada3368
Junyoung Choi <fluke8259@gmail.com>
2017-04-29 22:59:40 +0900

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ const Root = styled.footer`
 class Footer extends React.PureComponent {
   render () {
     return <Root>
-      Copyright 2017. Junyoung Choi. All rights reserved.
+      Copyright 2017 Junyoung Choi. <a href='https://github.com/CarbonStack/carbonstack' target='_blank'>Source code</a> is published under MIT.
     </Root>
   }
 }
