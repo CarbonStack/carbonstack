f2400915ceeba98573b3b9d5e97dfd27180dcbd7
Junyoung Choi <fluke8259@gmail.com>
2017-04-30 01:22:01 +0900

    Add style to Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import styled from 'styled-components'
+import media, { largeBreakpoint } from '../lib/styles/media'
+import { monospacedFontFamily } from '../lib/styles/variables'
+
 
 const Root = styled.footer`
-
+  height: 60px;
+  display: flex;
+  width: 100%;
+  max-width: ${largeBreakpoint}em;
+  margin: 35px auto 0;
+  font-family: ${monospacedFontFamily};
+  p {
+    width: 80%;
+    margin: 0 auto;
+    padding: 0 15px;
+    ${media.small`
+      width: 100%;
+    `}
+  }
 `
 
 class Footer extends React.PureComponent {
   render () {
     return <Root>
-      Copyright 2017 Junyoung Choi. <a href='https://github.com/CarbonStack/carbonstack' target='_blank'>Source code</a> is published under MIT.
+      <p>
+        Copyright 2017 Junyoung Choi.&nbsp;<a href='https://github.com/CarbonStack/carbonstack' target='_blank'>Source code of Carbonstack</a>&nbsp;is published under MIT.
+      </p>
     </Root>
   }
 }
