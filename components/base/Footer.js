af6776fc87f7a18c18355f40e58364e5ca3237b7
Junyoung Choi <fluke8259@gmail.com>
2017-06-06 19:52:13 +0900

    Fix stye bug of footer again

--- components/base/Footer.js
+++ components/base/Footer.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 class Footer extends React.PureComponent {
   render () {
-    return <footer className='container row'>
-      <p className='col-xs-12'>
+    return <footer>
+      <p>
         Copyright 2017 Junyoung Choi.&nbsp;<a href='https://github.com/CarbonStack/carbonstack' target='_blank'>Source code of Carbonstack</a>&nbsp;is published under MIT.
       </p>
       <style jsx>{`
         p {
           text-align: center;
-          margin: 0 auto;
-          line-height: 60px;
+          padding: 15px 0;
         }
       `}</style>
     </footer>
